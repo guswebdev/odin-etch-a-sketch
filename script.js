b7e7aea12175bd65d16de6eeed82e2e9d7b54fd4
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-07-13 07:23:25 -0300

    Creacion del evento de desplazamiento en la grilla

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+const d = document;
+const $cuadricula = d.querySelector("[data-cuadricula]");
+
+let mouseover = (e) => {
+  if (e.target.classList.contains("grilla")) {
+    e.target.classList.add("bg-hover");
+  }
+};
+
+let app = () => {
+  console.log($cuadricula);
+  for (let i = 0; i < 16 * 16; i++) {
+    let grilla = d.createElement("div");
+    grilla.className = "grilla";
+    $cuadricula.appendChild(grilla);
+  }
+
+  d.addEventListener("mouseover", mouseover);
+};
+
+d.addEventListener("DOMContentLoaded", app);
