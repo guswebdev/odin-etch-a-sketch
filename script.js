832910e67af47adcc6796fe968fa6065667cb508
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-10-06 08:17:14 -0300

    Refactorizacion de proyecto a clases JS

--- script.js
+++ script.js
@@ -1,57 +1,115 @@
 const d = document;
-let tamaño = "";
-const $cuadricula = d.querySelector("[data-cuadricula]");
-const $btnGrilla = d.querySelector("[data-generar-grilla]");
 
-let crearCuadricula = (tamaño = 16) => {
-  for (let i = 0; i < tamaño * tamaño; i++) {
-    let grilla = d.createElement("div");
-    grilla.className = "grilla";
-    let divisionGrilla = 512 / tamaño;
-    grilla.style.flexBasis = `${divisionGrilla}px`;
-    $cuadricula.appendChild(grilla);
+class Grilla {
+  tamaño = 16;
+  width;
+  height;
+
+  get tamaño() {
+    return this.tamaño;
   }
-};
+  set tamaño(nuevoTamaño) {
+    this.tamaño = nuevoTamaño;
+  }
+  get width() {
+    return this.width;
+  }
+  set width(nuevoWidth) {
+    this.tamaño = nuevoWidth;
+  }
+  get height() {
+    return this.height;
+  }
+  set tamaño(nuevoHeight) {
+    this.tamaño = nuevoHeight;
+  }
+}
+class Display {
+  $cuadricula = d.querySelector("[data-cuadricula]");
+  $btnGrilla = d.querySelector("[data-generar-grilla]");
+  $mensajeTamaño = d.querySelector("[data-tamaño]");
+  $inputTamaño = d.getElementById("tamanio");
+  $templateCelda = d.getElementById("template-grilla").content;
+  $fragment = d.createDocumentFragment();
 
-let generarGrilla = () => {
-  let tamañoCorrecto = true;
+  crearCuadricula() {
+    for (let i = 0; i < grilla.tamaño * grilla.tamaño; i++) {
+      let clone = this.$templateCelda.cloneNode(true);
+      this.$fragment.appendChild(clone);
+    }
+    this.$cuadricula.appendChild(this.$fragment);
+    this.actualizarDatos(this.$mensajeTamaño, grilla.tamaño);
+  }
 
-  while (tamañoCorrecto) {
-    tamaño = prompt("Ingrese el tamaño de la grilla (1-100): ");
-    if (tamaño > 0 && tamaño <= 100) {
-      tamañoCorrecto = false;
+  limpiarCuadricula() {
+    this.$cuadricula.innerHTML = "";
+  }
+
+  actualizarDatos(nodo, dato) {
+    nodo.textContent = "";
+    nodo.textContent = dato;
+  }
+
+  mostrarMensajeValidacion() {
+    const $mensajeValidacion = d.createElement("span");
+    $mensajeValidacion.textContent = "Valor Incorrecto, Intente de nuevo";
+    this.$inputTamaño.parentElement.appendChild($mensajeValidacion);
+  }
+}
+class Controlador {
+  guardarDimensionesGrilla() {
+    grilla.width = getComputedStyle(display.$cuadricula).width;
+    grilla.height = getComputedStyle(display.$cuadricula).height;
+  }
+
+  crearAreaCuadricula() {
+    display.$cuadricula.style.gridTemplateColumns = `repeat(auto-fill, minmax(${
+      grilla.width.slice(0, -2) / grilla.tamaño
+    }px, 1fr))`;
+
+    display.$cuadricula.style.gridTemplateRows = `repeat(auto-fill, minmax(${
+      grilla.width.slice(0, -2) / grilla.tamaño
+    }px, 1fr))`;
+  }
+
+  domContentLoaded() {
+    controlador.guardarDimensionesGrilla();
+    controlador.crearAreaCuadricula();
+    display.crearCuadricula();
+  }
+
+  click(e) {
+    if (e.target === display.$btnGrilla) {
+      //Crear una validacion para numero entre 1 y 100
+      if (display.$inputTamaño.value > 0 && display.$inputTamaño.value <= 100) {
+        grilla.tamaño = display.$inputTamaño.value;
+        display.limpiarCuadricula();
+        controlador.crearAreaCuadricula();
+        display.crearCuadricula();
+      } else {
+        console.log("ES UN NUMERO INCORRECTO");
+        display.mostrarMensajeValidacion();
+      }
     }
   }
 
-  console.log(tamaño);
-  $cuadricula.innerHTML = "";
-  crearCuadricula(tamaño);
-};
-
-let mouseover = (e) => {
-  if (e.target.classList.contains("grilla")) {
-    e.target.style.backgroundColor = `rgb(${Math.floor(
-      Math.random() * 256
-    )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  mouseover(e) {
+    /*
+    if (e.target.classList.contains("grilla")) {
+      e.target.style.backgroundColor = `rgb(${Math.floor(
+        Math.random() * 256
+      )},${Math.floor(Math.random() * 256)},${Math.floor(
+        Math.random() * 256
+      )})`;
+    }
+    */
   }
-};
+}
 
-let click = (e) => {
-  if (e.target.classList.contains("btn-grilla")) {
-    generarGrilla();
-  }
-};
+const grilla = new Grilla();
+const display = new Display();
+const controlador = new Controlador();
 
-let app = () => {
-  console.log($cuadricula);
-  for (let i = 0; i < 16 * 16; i++) {
-    let grilla = d.createElement("div");
-    grilla.className = "grilla";
-    $cuadricula.appendChild(grilla);
-  }
-
-  d.addEventListener("mouseover", mouseover);
-  d.addEventListener("click", click);
-};
-
-d.addEventListener("DOMContentLoaded", app);
+d.addEventListener("DOMContentLoaded", controlador.domContentLoaded);
+d.addEventListener("click", controlador.click);
+d.addEventListener("mouseover", controlador.mouseover);
